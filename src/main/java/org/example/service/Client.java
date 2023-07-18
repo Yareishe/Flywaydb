0ab8a822fd66a1b5ae6c0a56227bc6132b22edef
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-19 00:21:20 +0300

    Create Client.java

---
+++ src/main/java/org/example/service/Client.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+public class Client {
+    private int id;
+    private String name;
+
+    public Client(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return " " + id + " " + name + '\n';
+    }
+}
